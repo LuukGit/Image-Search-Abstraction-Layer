541129bbd8e7d2b9ce7a4df9567b94f896ed4a4a
Luuk <luukvoorbrowsen@gmail.com>
2016-03-15 15:53:27 +0000

    Google Search working

---
+++ client/js/functions.js
@@ -0,0 +1,73 @@
+var mongo = require("mongodb").MongoClient;
+var mongoURL = "mongodb://client:client@ds013579.mlab.com:13579/freecodecamp-database";
+
+module.exports = {
+    parseResults:   function parseResults(response)
+                    {
+                        var items = response.items;
+                        var objects = [];
+                        if (items !== undefined)
+                        {
+                            for (var i = 0; i < items.length; i++)
+                            {
+                                if (response.items[i].pagemap.cse_image)
+                                {
+                                    var url = response.items[i].pagemap.cse_image[0];
+                                }
+                                else
+                                {
+                                    url = "Not Found";
+                                }
+                                objects.push({url: url, snippet: response.items[i].snippet, context: response.items[i].link});
+                            }
+                        }
+                        else 
+                        {
+                            objects.push({error: "No results found."});
+                        }
+                        return objects;
+                    },
+        
+    initQueries:    function initQueries(latestQueries)
+                    {
+                        mongo.connect(mongoURL, function(err, db) {
+                            if (err) { throw err; }
+                            
+                            var cursor = db.collection("queries").find();
+                            cursor.each(function(err, docs) {
+                                if (err) { throw err; }
+                                if (docs !== null)
+                                {
+                                    console.log(docs);
+                                    latestQueries = docs.queries;
+                                    db.close();
+                                }
+                            });
+                        });       
+                    },
+        
+    saveQuery:      function saveQuery(query, latestQueries)
+                    {
+                        latestQueries.push({term: query, when: new Date().toISOString()});
+                        if (latestQueries.length > 10)
+                        {
+                            latestQueries.pop();
+                        }
+                        console.log(latestQueries);
+                        var queries = latestQueries;
+                        
+                        mongo.connect(mongoURL, function(err, db) {
+                            if (err) { throw err; }
+                            db.collection("queries").replaceOne({
+                                
+                            },{
+                                queries  
+                            }, function(err, data) {
+                                if (err) { throw err; }
+                                console.log(data);
+                                db.close();
+                                return queries;
+                            });
+                        });
+                    }
+}
--- server.js
+++ server.js
@@ -1,84 +1,63 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
+/*
+User Story: I can get the image URLs, alt text and page urls for a set of images relating to a given search string.
+User Story: I can paginate through the responses by adding a ?offset=2 parameter to the URL.
+User Story: I can get a list of the most recently submitted search strings.
+
+cx: 004013124895533866227:pi661a3hh0e
+API Key: AIzaSyDMaaMGWQBtufrRh3BPqInmLvuj9MFL8ys 
+*/
+
 var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var functions = require("./client/js/functions.js");
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
+var app = express();
+var server = http.createServer(app);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+var GoogleSearch = require('google-search');
+var googleSearch = new GoogleSearch({
+  key: "AIzaSyDMaaMGWQBtufrRh3BPqInmLvuj9MFL8ys",
+  cx: "004013124895533866227:pi661a3hh0e"
+});
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
+var latestQueries = [];
 
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
+app.get("/api/imagesearch/:query*", function(req, res) {
+    var offset = 1;
+    var query = req.url.replace("/api/imagesearch/", "").split("%20").join(" ");
+    functions.saveQuery(query, latestQueries);
+    
+    // Handle offset option
+    if (query.match(/\?offset=/))
+    {
+        offset = query.split("=")[1];
+        if (offset > 10) 
+        { 
+            offset = 10; 
+        }
+        else if (offset < 1)
+        {
+            offset = 1;
+        }
+        query = query.split("?")[0];
     }
-  );
-}
+    
+    googleSearch.build({
+      q: query,
+      fileType: "jpg",
+      num: offset 
+    }, function(error, response) {
+        if (error) { throw error; }
+          res.send(functions.parseResults(response));
+    });
+});
 
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
+app.get("/api/latest/imagesearch", function(req, res) {
+    res.send(latestQueries);
+});
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+    functions.initQueries(latestQueries);
+    var addr = server.address();
+    console.log("App listening at", addr.address + ":" + addr.port);
 });
